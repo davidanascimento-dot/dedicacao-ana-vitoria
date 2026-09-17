776ec9211f5bfe8ddcf4dec665ff082266a55aed
davidanascimento-dot <david.a.nascimento@ba.estudante.senai.br>
2026-09-17 12:35:40 -0300

    comit

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+
+app.use(express.json());
+
+
+app.post(req,res) =>{
+ 
+}
+
+
+app.listen(PORT, () => {
+console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
