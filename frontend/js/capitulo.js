12d8176d220c4b24dec997e4f9b4edbfc1839b18
davidanascimento-dot <david.a.nascimento@ba.estudante.senai.br>
2026-09-17 21:58:28 -0300

    comit

---
+++ frontend/js/capitulo.js
@@ -0,0 +1,43 @@
+
+const resposta = await fetch('js/capitulo.json')
+const capitulos = await resposta.json()
+
+
+const params = new URLSearchParams(window.location.search)
+const numero = Number(params.get('numero')) || 1
+
+
+const capitulo = capitulos.find(cap => cap.numero === numero)
+
+
+if (!capitulo) {
+  document.querySelector('#titulo-capitulo').textContent = 'Capítulo não encontrado'
+} else {
+  document.querySelector('#titulo-capitulo').textContent = `Capítulo ${capitulo.numero} — ${capitulo.titulo}`
+  document.querySelector('#conteudo-capitulo').textContent = capitulo.conteudo
+}
+
+
+const btnAnterior = document.querySelector('#btn-anterior')
+btnAnterior.addEventListener('click',()=>{
+  if ( numero > 1 ) {
+    window.location.href = 'capitulo.html?numero=' + (numero - 1)
+  }
+  
+})
+
+
+
+
+
+const btnProximo = document.querySelector('#btn-proximo')
+
+btnProximo.addEventListener('click',()=>{
+  const existeProximo = capitulos.some(cap =>cap.numero === numero + 1 )
+
+  if (existeProximo) {
+    window.location.href = 'capitulo.html?numero=' + (numero + 1)
+  }
+
+})
+
